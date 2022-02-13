014f93d3733733e3688cd3fd52abf9e9693588da
Chetan Kini <chetan@superconductive.com>
2022-02-12 21:21:44 -0500

    feat: get file traversal and basic regex working

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"errors"
-	"flag"
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
+	"regexp"
 )
 
 func main() {
@@ -12,31 +15,73 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	files := getFilesToSearch(source)
-	searchFileList(files, query)
+	paths, err := getFilesToSearch(source)
+	if err != nil {
+		log.Fatal(err)
+	}
+	searchFileList(paths, query)
 }
 
 func parseArgs() (string, string, error) {
-	argv := flag.Args()
-	argc := len(argv)
-	if argc == 1 {
-		return argv[0], ".", nil
-	} else if argc == 2 {
-		return argv[0], argv[1], nil
+	args := os.Args[1:]
+	if len(args) == 1 {
+		return args[0], ".", nil
+	} else if len(args) == 2 {
+		return args[0], args[1], nil
 	}
-	return "", "", errors.New("A search query/pattern is mandatory")
+	return "", "", errors.New("Improper arguments; search query/pattern is mandatory")
 }
 
-func getFilesToSearch(source string) []string {
-	return []string{source}
-}
+func getFilesToSearch(source string) ([]string, error) {
+	f, err := os.Stat(source)
+	if err != nil {
+		return nil, err
+	}
 
-func searchFileList(files []string, query string) {
-	for _, file := range files {
-		searchFile(file, query)
+	if f.IsDir() {
+		return getFilePathsFromDir(source)
+	} else {
+		return []string{source}, nil
 	}
 }
 
-func searchFile(file, query string) {
-	fmt.Println(file, query)
+func getFilePathsFromDir(dir string) ([]string, error) {
+	paths := make([]string, 0)
+	err := filepath.Walk(dir,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			paths = append(paths, path)
+			return nil
+		})
+	return paths, err
+}
+
+func searchFileList(paths []string, query string) {
+	r, err := regexp.Compile(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, path := range paths {
+		searchFile(path, r)
+	}
+}
+
+func searchFile(path string, r *regexp.Regexp) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text := scanner.Text()
+		match := r.MatchString(text)
+		if match {
+			fmt.Println(path, text)
+		}
+	}
 }
